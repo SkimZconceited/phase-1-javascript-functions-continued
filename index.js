501f2f30db8188e70cead0bc7913a1057ab1eba6
Stanley 'SkimZ' Kimambo <Stanleykimambo@gmail.com>
2023-09-21 14:04:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+// First part
+function saturdayFun (x = 'roller-skate') {
+    return `This Saturday, I want to ${x}!`;
+}
+
+saturdayFun();
+saturdayFun('bathe my dog');
+
+// Second part 
+function mondayWork (x = 'go to the office') {
+    return `This Monday, I will ${x}.`;
+}
+
+mondayWork();
+mondayWork('work from home');
+
+// Third part
+function wrapAdjective (x = '*') {
+    function inner (y = 'special'){
+        return `You are ${x}${y}${x}!`;
+    }
+    return inner;
+}
+
+let result = wrapAdjective('*');
+let emphatic = result("a hard worker");
+result = wrapAdjective("||");
+emphatic = result("a dedicated programmer");
+console.log(wrapAdjective()());
+// wrapAdjective('*');
+// wrapAdjective("%")("a dedicated programmer");
